4a9cf6bc9d203b5881329ea12619690c76bb838a
sourabh <sourabhs@espsofttech.org>
2024-09-09 16:13:14 +0530

    intial commit

---
+++ DigiPhyNFTbackend/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    
+
+    
+    Network:"MAINNET", //Please open for mainnet blockchain
+    nftMetadataUrl : "https://api.digiphynft.shop/api/nft/metadata/",
+
+    mysqlPort: 3306,
+
+    JWT_SECRET_KEY: '6dqw7dydyw7ewyuw',
+    SESSION_EXPIRES_IN: '24h', // Session will expire after 1 day of creation
+
+    
+    
+    // walletApiUrl: "https://espsofttech.in:8001/api/erc1155/mainnet/createWallet",
+    blockchainApiUrl: 'https://espsofttech.in:8001/api/erc1155/matictest/', //testnet
+    ethTransferApiUrl: 'https://espsofttech.in:8001/api/matic/testnet/transfer',
+    blockchainNetwork: 'testnet',
+    mailUrl: 'https://digiphynft.shop/',
+    pinata_api_key:"105327714c080a01a4b5",
+    pinata_secret_api_key:"e18cf3c1a8a7376852a4674735896bda9b7870cb4e11cc05c9e614711f955b35",
+
+
+}
+
